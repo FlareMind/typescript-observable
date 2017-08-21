68e7978aff757640ffe8b219277d9aef0d372bf0
afsa <mattulainen@gmail.com>
2017-08-21 19:35:52 +0200

    Improved export

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,12 @@
-import {Observable} from "./observable";
-export default Observable;
+
+// Classes
+export {Observable} from 'observable'
+export {ChangeObservable} from 'change-observable'
+
+
+// Interfaces
+export {IObservable, IChangeObservable} from 'interfaces/observable'
+export {IObserver} from 'interfaces/observer'
+export {IObservableEvent} from 'interfaces/observable-event'
+export {ICancel} from 'interfaces/cancel'
+
